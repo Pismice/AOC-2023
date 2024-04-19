4f677bd8456f2286a136a6de1aa8716ec00c0f79
JSantoro <jeremie.santoro@heig-vd.ch>
2024-04-19 02:01:52 +0200

    day 2 part 2 finished

--- day2/src/main.zig
+++ day2/src/main.zig
@@ -6,7 +6,7 @@ pub fn main() !void {
 
     const Color = struct {
         name: []const u8,
-        max: u8,
+        max: u32,
     };
 
     const COLORS = [_]Color{
@@ -20,20 +20,23 @@ pub fn main() !void {
     // Une Game
     var index: u32 = 0;
     var result: u32 = 0;
+    var result2: u32 = 0;
     while (try iter.next()) |raw_line| {
         var isValidGame: bool = true;
         var squares_picked: u8 = undefined;
         var color: Color = undefined;
+        var maxes: [3]Color = [_]Color{ .{ .name = "red", .max = 0 }, .{ .name = "green", .max = 0 }, .{ .name = "blue", .max = 0 } };
         defer {
             index += 1;
             std.debug.print("Game {} is valid = {}\n", .{ index, isValidGame });
             if (isValidGame) {
                 result += index;
             }
+            result2 += maxes[0].max * maxes[1].max * maxes[2].max;
         }
         const line = remove_til(raw_line, ':');
         var prevIsNumber: bool = false;
-        outer: for (line, 0..) |current_char, i| {
+        for (line, 0..) |current_char, i| {
             if (prevIsNumber) {
                 prevIsNumber = false; // assume qu on aura jamais de centaine 100
                 if (current_char <= '9' and current_char >= '0') {
@@ -58,9 +61,25 @@ pub fn main() !void {
                         if (std.mem.startsWith(u8, bout_restant, coloro.name)) {
                             color = coloro;
                             std.debug.print("Found color = {s} with {d} squares\n", .{ color.name, squares_picked });
+
+                            if (std.mem.startsWith(u8, color.name, "red")) {
+                                if (maxes[0].max < squares_picked) {
+                                    maxes[0].max = squares_picked;
+                                }
+                            } else if (std.mem.startsWith(u8, color.name, "green")) {
+                                if (maxes[1].max < squares_picked) {
+                                    maxes[1].max = squares_picked;
+                                }
+                            } else if (std.mem.startsWith(u8, color.name, "blue")) {
+                                if (maxes[2].max < squares_picked) {
+                                    maxes[2].max = squares_picked;
+                                }
+                            }
+
                             if (squares_picked > color.max) {
                                 isValidGame = false;
-                                break :outer;
+                                // was only useful for performance in part 1
+                                //                                break :outer;
                             }
                         }
                     }
@@ -68,7 +87,8 @@ pub fn main() !void {
             }
         }
     }
-    std.debug.print("Result = {}\n", .{result});
+    std.debug.print("Result part 1 = {}\n", .{result});
+    std.debug.print("Result part 2 = {}\n", .{result2});
 }
 
 fn remove_til(s: []const u8, c: u8) []const u8 {
